3018389740e6730f73784467b9b217d5d176fd9c
Amalya <eminamalya@gmail.com>
2021-02-27 18:04:59 +0400

    Django Proje2 uygulamam

--- djangoprojesi2/settings.py
+++ djangoprojesi2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'bn52!artl*a%%2rn%ypgw$3$zq4hz5s0@-qq=6blp0^sn8gnu('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'djangoprojesi2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR),'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+if DEBUG:
+    STATICFILES_DIRS=[
+        os.path.join(BASE_DIR,"static")
+    ]
+else:
+     STATIC_ROOT =os.path.join(BASE_DIR,"static")
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- djangoprojesi2/urls.py
+++ djangoprojesi2/urls.py
@@ -10,7 +10,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from blogSitesi import views
 
+from django.conf import settings
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.gonderi_listesi, name='gonderi_listesi'),
+
 ]
+
+
